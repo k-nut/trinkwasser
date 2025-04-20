ba2b8fecd9a82ed9949e66659227594a6dbccee9
Knut Hühne <knut@k-nut.eu>
2025-04-20 16:57:08 +0200

    Update to latest data

--- src/data/zones.js
+++ src/data/zones.js
@@ -1,93 +1,93 @@
 tw.data.zones = {
   "Friedrichshagen": {
-    "magnesium": "11.9",
-    "sulfat": "162",
-    "natrium": "39",
-    "chlorid": "61",
-    "calcium": "107",
-    "kalium": "5.1",
-    "nitrat": "2.9",
-    "hardness": "17.7"
-  },
-  "Kaulsdorf": {
-    "magnesium": "12.4",
-    "sulfat": "107",
-    "natrium": "22",
-    "chlorid": "46",
-    "calcium": "115",
-    "kalium": "5.3",
-    "nitrat": "1.4",
-    "hardness": "18.9"
-  },
-  "Tiefwerder": {
-    "magnesium": "10.3",
-    "sulfat": "144",
-    "natrium": "58",
-    "chlorid": "90",
-    "calcium": "125",
-    "kalium": "4.8",
-    "nitrat": "3.6",
+    "magnesium": "13.0",
+    "sulfat": "165",
+    "natrium": "40",
+    "chlorid": "64",
+    "calcium": "121",
+    "kalium": "5.2",
+    "nitrat": "4.31",
     "hardness": "19.9"
   },
+  "Kaulsdorf": {
+    "magnesium": "13.1",
+    "sulfat": "104",
+    "natrium": "23",
+    "chlorid": "48",
+    "calcium": "118",
+    "kalium": "5.0",
+    "nitrat": "1.86",
+    "hardness": "19.5"
+  },
+  "Tiefwerder": {
+    "magnesium": "11.6",
+    "sulfat": "146",
+    "natrium": "67",
+    "chlorid": "94",
+    "calcium": "138",
+    "kalium": "4.8",
+    "nitrat": "4.21",
+    "hardness": "22"
+  },
   "Wuhlheide": {
-    "magnesium": "15.2",
-    "sulfat": "190",
-    "natrium": "33",
-    "chlorid": "52",
-    "calcium": "141",
-    "kalium": "7.9",
-    "nitrat": "4.4",
-    "hardness": "23.2"
+    "magnesium": "15.9",
+    "sulfat": "173",
+    "natrium": "32",
+    "chlorid": "50",
+    "calcium": "144",
+    "kalium": "8.0",
+    "nitrat": "2.72",
+    "hardness": "23.9"
   },
   "Spandau": {
-    "magnesium": "7.8",
-    "sulfat": "72",
-    "natrium": "25",
-    "chlorid": "38",
-    "calcium": "94",
-    "kalium": "3.4",
-    "nitrat": "1.1",
-    "hardness": "14.9"
+    "magnesium": "8.1",
+    "sulfat": "60",
+    "natrium": "27",
+    "chlorid": "44",
+    "calcium": "91",
+    "kalium": "3.5",
+    "nitrat": "1.02",
+    "hardness": "14.6"
   },
   "Tegel": {
-    "magnesium": "9.7",
-    "sulfat": "116",
-    "natrium": "39",
-    "chlorid": "54",
-    "calcium": "101",
-    "kalium": "7.6",
-    "nitrat": "4.9",
-    "hardness": "16.3"
+    "magnesium": "9.6",
+    "sulfat": "89",
+    "natrium": "37",
+    "chlorid": "53",
+    "calcium": "92",
+    "kalium": "7,3",
+    "nitrat": "2.83",
+    "hardness": "15.0"
   },
   "Beelitzhof": {
-    "magnesium": "8.7",
-    "sulfat": "99",
-    "natrium": "42",
-    "chlorid": "58",
-    "calcium": "98",
-    "kalium": "3.3",
-    "nitrat": "3.3",
-    "hardness": "15.6"
+    "magnesium": "9.3",
+    "sulfat": "98",
+    "natrium": "49",
+    "chlorid": "69",
+    "calcium": "105",
+    "kalium": "3.6",
+    "nitrat": "3.82",
+    "hardness": "16.9"
   },
   "Kladow": {
-    "magnesium": "8.7",
-    "sulfat": "120",
-    "natrium": "31",
-    "chlorid": "44",
-    "calcium": "106",
-    "kalium": "2.9",
-    "nitrat": "2.7",
-    "hardness": "16.7"
+    "magnesium": "10.2",
+    "sulfat": "128",
+    "natrium": "40",
+    "chlorid": "57",
+    "calcium": "116",
+    "kalium": "3.9",
+    "nitrat": "2.97",
+    "hardness": "18.5"
   },
   "Stolpe": {
     "magnesium": "9.9",
-    "sulfat": "71",
-    "natrium": "28",
-    "chlorid": "44",
-    "calcium": "93",
-    "kalium": "3.7",
-    "nitrat": "3.4",
-    "hardness": "15.2"
+    "sulfat": "58",
+    "natrium": "27",
+    "chlorid": "42",
+    "calcium": "92",
+    "kalium": "3,7",
+    "nitrat": "3.23",
+    "hardness": "15.1"
   }
 };
 tw.data.averageValues = { "natrium": 8.8, "kalium": 1.6, "calcium": 77.7, "magnesium": 17.9, "chlorid": 18.2, "nitrat": 14.9, "sulfat": 51.5, "hardness": 14.9 };
